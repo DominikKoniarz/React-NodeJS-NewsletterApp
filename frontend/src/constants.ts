a81db2b1e9f1f63bb172450bbd12f4e8c0293358
DominikKoniarz <dominik.koniarz.development@gmail.com>
2023-12-16 18:11:12 +0100

    Done dynamic base_url from .env

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.PROD
+	? import.meta.env.BASE_URL
+	: "http://localhost:3000";
 
 export const REGISTER_URL = `${BASE_URL}/register`;
 export const UNREGISTER_URL = `${BASE_URL}/unregister`;

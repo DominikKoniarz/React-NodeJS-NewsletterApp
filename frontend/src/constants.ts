9effa6a5eb58aa8166262d63b12770bf82274d38
DominikKoniarz <dominik.koniarz.development@gmail.com>
2023-12-16 19:00:52 +0100

    Frontend env fix

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.PROD
-	? import.meta.env.SERVER_URL
+	? import.meta.env.VITE_SERVER_URL
 	: "http://localhost:3000";
 
 export const REGISTER_URL = `${BASE_URL}/register`;
